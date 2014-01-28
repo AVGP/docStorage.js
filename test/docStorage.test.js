08e775e218a57b5026ad74683ba40a09dfc4c4e1
Martin N <mr.avgp@googlemail.com>
2014-01-28 02:00:28 -0500

    Ah, forgot to commit the tests. Here they are.

--- test/docStorage.test.js
+++ test/docStorage.test.js
@@ -1,10 +1,32 @@
 describe("docStorage", function() {
   it("is exported into the window scope", function() {
     expect(window.docStorage).toBeTruthy();
-  })
+  });
   
   it("can save a key/value pair", function() {
+    window.docStorage.registerBackend(localStorageBackend);
     window.docStorage.save("test.txt", "Hello World!");
     expect(window.docStorage.load("test.txt").content).toBe("Hello World!");
-  })
+  });
+  
+  it("can use arbitrary storage backends", function() {
+     var backend = {
+        isAvailable: function() { return true; },
+        save: function() {},
+        load: function() { return {version: 1}; }
+     };
+     
+     spyOn(backend, "isAvailable").andCallThrough();
+     spyOn(backend, "save");
+     spyOn(backend, "load").andCallThrough();;
+     
+     window.docStorage.registerBackend(backend);
+     window.docStorage.save("test.txt", "Hello World");
+     window.docStorage.load("test.txt");
+     
+     expect(backend.isAvailable).toHaveBeenCalled();
+     expect(backend.save).toHaveBeenCalled();
+     expect(backend.load).toHaveBeenCalled();
+      
+  });
 });
