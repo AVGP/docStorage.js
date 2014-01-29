6e5913c8e396967feac593330ed9de306f6f8460
Martin N <mr.avgp@googlemail.com>
2014-01-29 01:34:32 -0500

    Adding listFiles and putting all docs in one root node in localStorage

--- backends/localStorage.js
+++ backends/localStorage.js
@@ -1,12 +1,24 @@
 var localStorageBackend = {
   isAvailable: function() { return true; },
   load: function(name) {
-    return JSON.parse(localStorage.getItem(name));
+    return JSON.parse(localStorage.getItem("docs"))[name];
   },
   save: function(name, content) {
-    localStorage.setItem(name, JSON.stringify({
+    var docs = JSON.parse(localStorage.getItem("docs")) || {};
+
+    docs[name] = {
       version: new Date().getTime(),
       content: content
-    }));  
+    };
+    
+    localStorage.setItem("docs", JSON.stringify(docs));  
+  },
+  listFiles: function() {
+    var files = [];
+    var docs = JSON.parse(localStorage.getItem("docs"));
+    for(var file in docs) {
+      if(docs.hasOwnProperty(file)) files.push(file);
+    }
+    return files;
   }
 };
